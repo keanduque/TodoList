92064755b43c3a19561e1ce2caaa8ce1045cc26b
Kean Duque <vizen27@users.noreply.github.com>
2023-03-29 18:26:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const { connectToDB, Collection } = require(__dirname + "/public/db.js");
 
 // init app & middleware
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
